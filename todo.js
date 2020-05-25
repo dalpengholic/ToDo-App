3327865bcafa00e6ffbd36adc3af26a5929854f6
Woojae Gabriel Lim <dalpengholic@gmail.com>
2020-05-25 23:12:36 +0300

    refactoring 2 need to fix ul

--- todo.js
+++ todo.js
@@ -1,8 +1,20 @@
 const toDoform = document.querySelector(".js-toDoForm"),
     toDoInput = toDoform.querySelector("input"),
+    toDoAdvice = document.querySelector(".js-advice"),
     toDoList = document.querySelector(".js-toDoList");
+    
 
 const TODOS_LS = 'toDos';
+const HIDING_CN = 'hiding';
+const LIST_ID = makeCounter();
+
+function makeCounter() {
+    var i = 0;
+    return function() {
+        return i++;
+    }
+}
+
 
 let toDos = [];
 
@@ -16,18 +28,30 @@ function deleteToDo(event){
     toDos = cleanToDos
     saveToDos();
 
+    const numList = countToDoList();
+    toDoform.classList.toggle(HIDING_CN, numList >= 5);
 }
 
 function saveToDos(){
     localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
 }
 
+function makeCounter() {
+    var i = 0;
+    return function() {
+        return i++;
+    }
+}
+
+
 function paintToDo(text){
     const li = document.createElement("li");
+    li.className = "js-toDoList_todo";
     const delBtn = document.createElement("button");
+    delBtn.className = "js-toDoList_delBtn";
     const newId = toDos.length + 1;
     const span = document.createElement("span");
-    delBtn.innerText = "X";
+    delBtn.innerText = "❌";
     delBtn.addEventListener("click", deleteToDo);
     span.innerText = text;
     li.appendChild(delBtn);
@@ -42,13 +66,22 @@ function paintToDo(text){
     saveToDos();
 }
 
+function countToDoList(){
+    const loadedToDos = localStorage.getItem(TODOS_LS);
+    const parsedToDos = JSON.parse(loadedToDos);
+    return parsedToDos.length
+}
+
 function handleSubmit(event){
     event.preventDefault();  
     const currentValue = toDoInput.value;
     paintToDo(currentValue);
     toDoInput.value = "";
-} 
+    
+    const numList = countToDoList();
+    toDoform.classList.toggle(HIDING_CN, numList >= 5);
 
+} 
 
 function loadToDos(){
     const loadedToDos = localStorage.getItem(TODOS_LS);
@@ -57,7 +90,7 @@ function loadToDos(){
         parsedToDos.forEach(function(toDo){
             paintToDo(toDo.text);
         });
-    }
+    } 
 } 
 
 function init(){
